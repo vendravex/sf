45721721c47bb36dc1837d400fd91cdd90fee826
vendravex <vendravex@gmail.com>
2020-08-04 13:38:39 +0700

    Update tutorialartMultiple.js

--- tutorialartMultiple.js
+++ tutorialartMultiple.js
@@ -19,11 +19,11 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*5)+ 1);var xxx=null;
-if(x=="1"){xxx="/apa-itu-forex/"}
-if(x=="2"){xxx="/denah-rumah-minimalis-dengan-type-21/"}
+if(x=="1"){xxx="/customize-interior-design/"}
+if(x=="2"){xxx="/customize-interior-design/"}
 if(x=="3"){xxx="/customize-interior-design/"}
-if(x=="4"){xxx="/ini-cara-mudah-memilih-premi-asuransi-murah/"}
-if(x=="5"){xxx="/dasar-dasar-dari-pasar-forex/"}a_to_ve[i].href="https://tutorialart.bid"+xxx+"?site="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_ve[i].target="_blank";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="/customize-interior-design/"}
+if(x=="5"){xxx="/customize-interior-design/"}a_to_ve[i].href="https://tutorialart.bid"+xxx+"?site="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_ve[i].target="_blank";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
